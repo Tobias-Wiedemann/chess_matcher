e2b379ba3d0ed5f3f82ac87c43734b72b6d76b41
Tobias Wiedemann <towi00001@stud.uni-saarland.de>
2024-07-09 13:11:13 +0200

    Working for parsing and matching a player list

--- main.js
+++ main.js
@@ -22,10 +22,10 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow()
-  
+
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
+  })
 })
 
 app.on('window-all-closed', () => {
@@ -51,13 +51,13 @@ ipcMain.on('open-file', (event) => {
           return;
         }
         finalData = data;
-        event.reply('response-data', { message: `data: ${data}`});
+        event.reply('response-data', { message: `data: ${data}` });
         processPlayerList();
       });
-    } catch(error) {
-      event.reply('response-data', { message: `failed opening '${inputPath}'`});
+    } catch (error) {
+      event.reply('response-data', { message: `failed opening '${inputPath}'` });
     }
-    event.reply('response-data', { message: `'${inputPath}' is the opened file path`});
+    event.reply('response-data', { message: `'${inputPath}' is the opened file path` });
   }
 })
 
@@ -75,16 +75,16 @@ ipcMain.on('save-file', (event, data) => {
         }
       });
 
-    } catch(error) {
-      event.reply('response-data', { message: `failed saving '${resultPath}'`});
+    } catch (error) {
+      event.reply('response-data', { message: `failed saving '${resultPath}'` });
     }
-    event.reply('response-data', { message: `'${resultPath}' is the resulting file path`});
+    event.reply('response-data', { message: `'${resultPath}' is the resulting file path` });
   }
 })
 
 ipcMain.on('process', (event) => {
 
-  event.reply('response-data', { message: `'${event.data}' is the opened file path`});
+  event.reply('response-data', { message: `'${event.data}' is the opened file path` });
 })
 
 function processPlayerList() {
@@ -93,14 +93,14 @@ function processPlayerList() {
   let players = [];
   splitData.forEach((line) => {
     console.log(`line: ${line}`);
-    
+
     // cuts of anything after this delimiter in this line
-    let tuple = line.split("### END_OF_LINE ###");
+    let tuple = line.split("###");
     // looks for matches
-    tuple = tuple[0].split("---");
+    tuple = tuple[0].split("-");
     if (tuple.length === 1) {
       // player list case
-      tuple = tuple[0].split(",,,");
+      tuple = tuple[0].split(",");
       if (tuple.length > 1) {
         let eloFirstTuple = [parseFloat(tuple[1]), tuple[0]];
         players.push(eloFirstTuple);
@@ -110,30 +110,34 @@ function processPlayerList() {
 
     }
   });
-  
-  
+
+
   console.log("finalData:");
   console.log(finalData);
   console.log("players:");
   console.log(players);
-  
+
   players.sort((a, b) => a[0] - b[0]);
   console.log("sorted players:");
   console.log(players);
 
 
+  // assembly of the new matches
   finalData = "";
   let first = "";
   let second = "";
-  for (let i = 0; i < players.length; i++) {
-    if (first === "") {
-      first = players[i][1] + " ,,, " + players[i][0].toString();
-    } else {
-      second = players[i][1] + " ,,, " + players[i][0].toString();
-      finalData = finalData + first + " --- " + second +  + " ### END_OF_LINE ### " + "\n";
-      first = "";
-      second = "";
+  for (let i = 0; i < players.length; i = i + 2) {
+    if (i === players.length - 1) {
+      // odd number case
+      first = players[i][1] + ", " + players[i][0].toString();
+      finalData = finalData + first + " ### " + "\n";
+      break;
     }
+    first = players[i][1] + ", " + players[i][0].toString();
+    second = players[i][1] + ", " + players[i + 1][0].toString();
+    finalData = finalData + first + " - " + second + " ### " + "\n";
+    first = "";
+    second = "";
   }
 
   console.log("finalData:");
---
+++ make_player_list.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const fs = require('fs');
+
+let resultPath = "generated.txt";
+let finalData = "";
+
+for (let i = 0; i < 107; i++) {
+    finalData = finalData + "Player" + (Math.floor(Math.random() * 10000)).toString() + ", " + (Math.floor(Math.random() * 2300)).toString() + "\n";
+}
+
+try {
+    fs.writeFile(resultPath, finalData, err => {
+        if (err) {
+            console.error(err);
+        } else {
+            // file written successfully
+        }
+    });
+} catch (e) {
+    console.log(e);
+}
